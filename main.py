6b7fb7bb70763e6d56eeced9cf08316c418f091e
debar101 <142681467+debar101@users.noreply.github.com>
2024-02-18 18:37:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 print("I'm learning how to program.")
-print("Hello Mr. Fiskey, hope you see this. Not sure if I'm doing this assignment correctly. Some Feedback would be nice)
+print("Hello Mr Fiskey hope you see this Not sure if I'm doing this assignment correctly Some Feedback would be nice)
